0787f9b45f2b0bac7e65fb29bb675419045a2fef
Joseph Eid <joseph@Josephs-MacBook-Pro.local>
2020-11-07 11:31:36 +0000

    Script which can access page content

--- background.js
+++ background.js
@@ -1,19 +1,21 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
+// Create a rule that will show the page action when the conditions are met.
+const kMatchRule = {
+  // Declare the rule conditions.
+  conditions: [new chrome.declarativeContent.PageStateMatcher({
+    pageUrl: {hostEquals: 'developer.chrome.com'},
+  })],
+  // Shows the page action when the condition is met.
+  actions: [new chrome.declarativeContent.ShowPageAction()]
+}
 
+// Register the runtime.onInstalled event listener.
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
+  // Overrride the rules to replace them with kMatchRule.
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+    chrome.declarativeContent.onPageChanged.addRules([kMatchRule]);
   });
 });
--- popup.js
+++ popup.js
@@ -1,21 +1,35 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
-
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+function displayWords() {
+  chrome.storage.local.get(['words'], function(object) {
+    let pageList = document.getElementById('displayWords');
+    if (object.words) {
+      searchWords = object.words
+      for (var i = 0; i < searchWords.length; i++){
+        let listItem = document.createElement('li');
+        listItem.innerText = searchWords[i]
+        pageList.appendChild(listItem);
+      }
+    }
   });
-};
+}
+
+displayWords();
+
+document.getElementById('wordSubmit').onclick = function() {
+  let userWords = document.getElementById('userWords').value.trim();
+  chrome.storage.local.get(['words'], function(object) {
+    let newWords = object.words || [];
+    newWords.push(userWords);
+    chrome.storage.local.set({words: newWords});
+  })
+  chrome.tabs.executeScript(null, {
+    file: 'content_script.js'
+  });
+}
+
+document.getElementById('clearList').onclick = function() {
+  chrome.storage.local.clear();
+}
