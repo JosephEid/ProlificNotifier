f6988ce9d14c1ee211f05473ad6495dc188084e1
JosephEid <josephceid@gmail.com>
2020-11-07 19:14:41 +0000

    Send study count to background script

--- content_script.js
+++ content_script.js
@@ -1,8 +1,9 @@
 chrome.storage.local.get(function () {
   let elements = Array.from(document.querySelectorAll("div.study-info"));
+
   for (let element of elements) {
     element.style.backgroundColor = "#89eb34";
-    chrome.runtime.sendMessage({ newIconPath: "images/get_started16.png" });
-    console.log("hello");
   }
+
+  chrome.runtime.sendMessage({ studyCount: elements.length });
 });
