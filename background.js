9b5241e5700ec97bb2560f71a31def57652376cf
JosephEid <josephceid@gmail.com>
2020-11-09 18:24:03 +0000

    Add logo to change when study comes in

--- background.js
+++ background.js
@@ -1,18 +1,16 @@
-// Show page action on any page.
 const pageActionMatchRule = {
-  // Declare the rule conditions.
+  // Be alerted on any page
   conditions: [
     new chrome.declarativeContent.PageStateMatcher({
       pageUrl: {},
     }),
   ],
-  // Shows the page action when the condition is met.
   actions: [new chrome.declarativeContent.ShowPageAction()],
 };
 
 // Register the runtime.onInstalled event listener.
 chrome.runtime.onInstalled.addListener(function () {
-  // Overrride the rules to replace them with kMatchRule.
+  // Overrride the rules to replace them with pageActionMatchRule.
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([pageActionMatchRule]);
   });
@@ -29,11 +27,9 @@ window.setInterval(function () {
 }, 2000);
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(`${request.studyCount} studies found`);
-
   chrome.browserAction.setIcon({
     path: {
-      16: "images/get_started16.png",
+      16: `images/pronotif16_study.png`,
     },
   });
 });
--- content_script.js
+++ content_script.js
@@ -1,9 +1,5 @@
 chrome.storage.local.get(function () {
   let elements = Array.from(document.querySelectorAll("div.study-info"));
 
-  for (let element of elements) {
-    element.style.backgroundColor = "#89eb34";
-  }
-
   chrome.runtime.sendMessage({ studyCount: elements.length });
 });
